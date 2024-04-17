ae91bc21da9d51b5c9948849a8e1a14c13735209
sofiaanahi <sofiaanahirodas@gmail.com>
2024-04-17 16:04:46 -0300

    se agrego el grafico

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const outputText = document.querySelector('#output-text');
 // Constantes
 const ARRAY_LENGTH = 9;
 
+tfvis.visor().surface({ name: 'Surface', tab: 'Datos' });
+
 // Modelo
 const model = tf.sequential();
 
@@ -25,7 +27,7 @@ model.compile(
         loss: 'meanSquaredError',
         optimizer: 'sgd'
     });
-    
+
 // Eventos para entrenar
 buttonLearn.addEventListener('click', async () => {
 
@@ -39,13 +41,30 @@ buttonLearn.addEventListener('click', async () => {
     const xs = tf.tensor2d(INPUT, [INPUT.length, 1]);
     const ys = tf.tensor2d(OUTPUT, [OUTPUT.length, 1]);
 
-    await model.fit(xs, ys, { epochs: 500 });
+    const surface = { name: 'Surface', tab: 'Datos' };
+    const historyOnBatchEnd = [];
+    const historyOnEpochEnd = [];
+    await model.fit(xs, ys, {
+        epochs: 500, callbacks: {
+            onBatchEnd: async (batch, logs) => {
+                console.log(logs);
+                historyOnBatchEnd.push(logs);
+                tfvis.show.history(surface, historyOnBatchEnd, ['loss']);
+            },
+            onEpochEnd: (epoch, logs) => {
+                historyOnEpochEnd.push(logs);
+                tfvis.show.history(surface, historyOnEpochEnd, ['loss']);
+            },
 
+            onTrainEnd: () => {
+                isTrainingSpan.classList.add('visually-hidden');
+                trainingCompleteSpan.classList.remove('visually-hidden');
+                buttonLearn.disabled = true;
+                document.getElementById('predecir-section').classList.remove('visually-hidden');
+            }
 
-    isTrainingSpan.classList.add('visually-hidden');
-    trainingCompleteSpan.classList.remove('visually-hidden');
-    buttonLearn.disabled = true;
-    document.getElementById('predecir-section').classList.remove('visually-hidden');
+        }
+    });
 
 })
 
